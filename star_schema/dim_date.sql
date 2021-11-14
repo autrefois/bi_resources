5fb95be270ed9f51befff928e07303377b37a6d6
odette <odette.duma@gmail.com>
2021-11-14 13:05:33 +0200

    added dim_state to schema + test data

---
+++ star_schema/dim_date.sql
@@ -0,0 +1,47 @@
+[
+    {
+        "name": "date_id",
+        "type": "INTEGER",
+        "mode": "REQUIRED"
+    },
+    {
+        "name": "date",
+        "type": "DATE",
+        "mode": "NULLABLE"
+    },
+    {
+        "name": "datetime",
+        "type": "TIMESTAMP",
+        "mode": "NULLABLE"
+    },
+    {
+        "name": "week_start_date",
+        "type": "DATE",
+        "mode": "NULLABLE"
+    },
+    {
+        "name": "week_end_date",
+        "type": "DATE",
+        "mode": "NULLABLE"
+    },
+    {
+        "name": "month_start_date",
+        "type": "DATE",
+        "mode": "NULLABLE"
+    },
+    {
+        "name": "month_end_date",
+        "type": "DATE",
+        "mode": "NULLABLE"
+    },
+    {
+        "name": "quarter_start_date",
+        "type": "DATE",
+        "mode": "NULLABLE"
+    },
+    {
+        "name": "quarter_end_date",
+        "type": "DATE",
+        "mode": "NULLABLE"
+    }
+]
---
+++ test_data_generator/test_data_dim_date.sql
@@ -0,0 +1,15 @@
+INSERT INTO
+`music-streaming-332016.star_schema.dim_date`
+SELECT ROW_NUMBER() OVER() date_id,
+    date,
+    cast (date as timestamp) as datetime, 
+    DATE_ADD(DATE_TRUNC(DATE_SUB(date, INTERVAL 1 DAY), WEEK), INTERVAL 1 DAY) as week_start_date,
+    DATE_ADD(DATE_ADD(DATE_TRUNC(DATE_SUB(date, INTERVAL 1 DAY), WEEK), INTERVAL 1 DAY), INTERVAL 6 DAY) as week_end_date,
+    DATE_TRUNC(date, MONTH) as month_start_date,
+    DATE_SUB(DATE_TRUNC(DATE_ADD(date, INTERVAL 1 MONTH), MONTH), INTERVAL 1 DAY) as month_end_date,
+    DATE_TRUNC(date, QUARTER) as quarter_start_date,
+    DATE_SUB(DATE_TRUNC(DATE_ADD(date, INTERVAL 1 QUARTER), QUARTER), INTERVAL 1 DAY) as quarter_end_date
+FROM 
+       UNNEST(GENERATE_DATE_ARRAY('2017-01-01', '2022-01-01')) AS date;
+
+SELECT * FROM `music-streaming-332016.star_schema.dim_date` ;
